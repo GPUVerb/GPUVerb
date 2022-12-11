be32f18c01d9090040f06f280379f56e873e0f7f
Runshi Gu <justingu14053@gmail.com>
2022-12-11 17:16:28 -0500

    Some changes to the scene

--- Assets/Scripts/Debug/BlockMovement.cs
+++ Assets/Scripts/Debug/BlockMovement.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class BlockMovement : MonoBehaviour
 {
-    public float speed = 5;
     public bool startMovement = false;
+    public float speed = 5;
+    public float MovementRange = 3.0f;
+    public Vector3 direction = Vector3.zero;
+
+    private Vector3 startPosition;
+    private float positive = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = gameObject.transform.position;
     }
 
     // Update is called once per frame
@@ -17,7 +22,11 @@ public class BlockMovement : MonoBehaviour
     {
         if(startMovement)
         {
-            this.transform.position += new Vector3(Time.deltaTime * speed, 0.0f, 0.0f);
+            transform.position += direction * (speed * Time.deltaTime) * positive;
+            if ((transform.position - startPosition).magnitude >= MovementRange)
+            {
+                positive = positive > 0 ? -1.0f : 1.0f;
+            }
         }
     }
 }
